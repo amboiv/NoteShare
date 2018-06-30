3b54f016b4fee30bcc3bb4f545b0b0ebf3ab204d
amboiv <oivind.amble@gmail.com>
2018-06-30 13:32:42 +0200

    Added new + create actions to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,24 @@ class CoursesController < ApplicationController
         @courses = Course.all
     end
 
+    def new
+        @course = Course.new
+    end
+
+    # POST /notes
+    # POST /notes.json
+    def create
+        @course = Course.new(params.require(:course).permit(:name, :code, :credits, :description))
+
+        respond_to do |format|
+            if @course.save
+                format.html { redirect_to courses_path, notice: 'A new Course was successfully created.' }
+                format.json { render :show, status: :created, location: @course }
+            else
+                format.html { render :new }
+                format.json { render json: @course.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
 end
