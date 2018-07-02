7de005193098d3ed13dcbc27e404eeb3053441fd
amboiv <oivind.amble@gmail.com>
2018-07-02 22:34:33 +0200

    Added custom course_show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :courses
+  resources :courses, except: [:show]
+  get 'course/:id', to: 'courses#show', as: 'course_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
