1394753b527b091f439292c45d1b568d0e5ae34e
amboiv <oivind.amble@gmail.com>
2018-07-06 10:44:48 +0200

    Added defaults for course image and thumbnail in model (||=)

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,11 @@ class Course < ApplicationRecord
     validates_presence_of :name, :code, :credits, :main_image, :thumbnail
 
     has_many :notes
+
+
+    after_initialize :set_defaults
+    def set_defaults
+        self.main_image ||= "https://placeholdit.co//i/600x300?"
+        self.thumbnail ||= "https://placeholdit.co//i/300x150?"
+    end
 end
