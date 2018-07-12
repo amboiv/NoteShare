c82cb42297c077d7a7375e287a9134f74980ea07
amboiv <oivind.amble@gmail.com>
2018-07-12 20:51:32 +0200

    Refactored user field whitelist out of ApplicationController into its own concern

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,12 @@
+module DeviseWhitelist
+    extend ActiveSupport::Concern
+
+    included do
+        before_action :configure_permission_params, if: :devise_controller?
+    end
+
+    def configure_permission_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+end
