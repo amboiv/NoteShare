ef6485921a03a1b766cf4b11234f642b3fc76b63
amboiv <oivind.amble@gmail.com>
2018-07-14 15:44:11 +0200

    Added session tracking for different source links  as an exercise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'log_out' }
   resources :education_programs
   resources :courses, except: [:show]
+
   get 'course/:id', to: 'courses#show', as: 'course_show'
+  
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
