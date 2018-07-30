89a87138c1bb4c07cc9503fdcfd503fcce066fd1
amboiv <oivind.amble@gmail.com>
2018-07-30 18:23:06 +0200

    Fixed a bug in comments. lowered minimum comment length

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  mount ActionCable.server => '/cable'
   get ':id', to: 'courses#show', as: 'course_show'
   
 
@@ -43,7 +44,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  mount ActionCable.server => '/cable'
+  
   
 
  
